0a150e033b892d025d18bd8186a34e95fff35fd8
nada <nadakhelif61@gmail.com>
2021-12-25 21:23:26 +0100

    test nada

---
+++ src/components/formulaireStand/FormulaireStand.jsx
@@ -0,0 +1,51 @@
+
+import React from "react";
+import classes from "./formulaireStand.module.css";
+import Button from "../button/Button";
+import { Card } from "@mui/material";
+
+function FormulaireStand() {
+  return (
+    <Card style={{width:'40rem',height:'700rem'}}>
+    <div className={classes.center}>
+      <h1>s'inscrire maintenant</h1>
+      <p>
+        En prenant part a notre evenement , vous associez votre entreprise a une
+        operation de communivation ciblée et efficace. En effet,votre entreprise
+        bénéficiera d'une visibilité non seulement lors de la médiatisation de
+        l'évenement mais aussi le jour meme de l'évènement.
+      </p>
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <input type="text" placeholder="Votre Nom " required id="non" />
+          </div>
+          <div className={classes.control}>
+            <input
+              type="text"
+              placeholder="Telephone "
+              required
+              id="telephone"
+            />
+          </div>
+          <div className={classes.control}>
+            <input type="text" placeholder="Email " required id="email" />
+          </div>
+          <div className={classes.control}>
+            <input
+              type="text"
+              placeholder="Nom de lentreprise "
+              required
+              id="entreprise"
+            />
+          </div>
+          <div className={classes.button}>
+          <Button content="S'inscrire" />
+        </div>
+        
+      </form>
+    </div>
+    </Card>
+  );
+}
+
+export default FormulaireStand;
