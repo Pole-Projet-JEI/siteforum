80271f66d721a4e5a6454c524e0cd75b792e56d8
Naim <naim.dali23@gmail.com>
2021-12-29 10:55:30 +0100

    pouquoi rejoindre forum

--- src/components/ObtenirTicket/ObtenirTicket.jsx
+++ src/components/ObtenirTicket/ObtenirTicket.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import classes from "./ObtenirTicket.module.css";
-import Carreau from "../Carreau/Carreau";
+import ConnectWithoutContactIcon from "@mui/icons-material/ConnectWithoutContact";
+import CelebrationIcon from "@mui/icons-material/Celebration";
+import LayersIcon from "@mui/icons-material/Layers";
+import ControlPointDuplicateIcon from "@mui/icons-material/ControlPointDuplicate";
 
 export default function ObtenirTicket(props) {
   return (
@@ -15,27 +18,45 @@ export default function ObtenirTicket(props) {
         </p>
         <button> bla bla bla</button>
       </div>
-      <div className={classes.body}>
-        <Carreau
-          icon="ConnectWithoutContactIcon"
-          title="Réseautage"
-          description="Une panoplie d’entreprises sera présente afin de vous permettre d’élargir et de développer votre réseau professionnel de manière solide et étendue."
-        />
-        <Carreau
-          icon="ControlPointDuplicateIcon"
-          title="Opportunités"
-          description="On vous procure la chance de décrocher des offres de stages de fin d’études, contrats de professionnalisation et d’emploi à temps partiel.                "
-        />
-        <Carreau
-          icon="LayersIcon"
-          title="Interférents"
-          description="Faire appel à un conférencier pour aborder les thématiques d’entreprises est de plus en plus prisée au sein des différentes organisations."
-        />
-        <Carreau
-          icon="CelebrationIcon"
-          title="Ambiance"
-          description="Malgré le cadre professionnel on vous assure une ambiance chaleureuse et animée par différentes activités. Soyez nombreux et ne ratez pas cette expérience !"
-        />
+
+      <div className={classes.carreau}>
+        <h2> Réseautage </h2>
+        <ConnectWithoutContactIcon className={classes.icon} fontSize="large" />
+        <p>
+          Une panoplie d’entreprises sera présente afin de vous permettre
+          d’élargir et de développer votre réseau professionnel de manière
+          solide et étendue.
+        </p>
+      </div>
+
+      <div className={classes.carreau}>
+        <h2> Ambiance </h2>
+        <CelebrationIcon className={classes.icon} fontSize="large" />
+        <p>
+          Malgré le cadre professionnel on vous assure une ambiance chaleureuse
+          et animée par différentes activités. Soyez nombreux et ne ratez pas
+          cette expérience !
+        </p>
+      </div>
+
+      <div className={classes.carreau}>
+        <h2> Interférents </h2>
+        <LayersIcon className={classes.icon} fontSize="large" />
+        <p>
+          Faire appel à un conférencier pour aborder les thématiques
+          d’entreprises est de plus en plus prisée au sein des différentes
+          organisations.
+        </p>
+      </div>
+
+      <div className={classes.carreau}>
+        <h2> Opportunités </h2>
+        <ControlPointDuplicateIcon className={classes.icon} fontSize="large" />
+        <p>
+          On vous procure la chance de décrocher des offres de stages de fin
+          d’études, contrats de professionnalisation et d’emploi à temps
+          partiel.
+        </p>
       </div>
     </div>
   );
