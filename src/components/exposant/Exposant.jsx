f78e321bb7946cbe54b3e1163a316fc960ac29cc
MohamedBouarada <bouarada7.mb@gmail.com>
2022-11-12 16:45:22 +0100

    updating company logos

--- src/components/exposant/Exposant.jsx
+++ src/components/exposant/Exposant.jsx
@@ -22,24 +22,156 @@ export default function Exposant() {
     });
     
     },[]);
+
+    const data=[
+        {
+            name:"3S",
+            logo:"/exposant/3S.jpg"
+        },
+        {
+            name:"axe-finance",
+            logo:"/exposant/axe-finance.png"
+        },
+        {
+            name:"BEYONDPLANS",
+            logo:"/exposant/BEYONDPLANS.jpg"
+        },
+        {
+            name:"Coficab",
+            logo:"/exposant/Coficab.jpg"
+        },
+        {
+            name:"Euranova",
+            logo:"/exposant/Euranova.jpg"
+        },
+        {
+            name:"ElyaData",
+            logo:"/exposant/ElyaData.jpg"
+        },
+        {
+            name:"Expensya",
+            logo:"/exposant/Expensya.png"
+        },
+        {
+            name:"ey-logo-black",
+            logo:"/exposant/ey-logo-black.png"
+        },
+        {
+            name:"Faurecia-logo",
+            logo:"/exposant/Faurecia-logo.jpg"
+        },
+        {
+            name:"finlogik",
+            logo:"/exposant/finlogik.png"
+        },
+        {
+            name:"habemus",
+            logo:"/exposant/habemus.png"
+        },
+        {
+            name:"iliade",
+            logo:"/exposant/iliade.jpg"
+        },
+        {
+            name:"infor",
+            logo:"/exposant/infor.png"
+        },
+        {
+            name:"integrationObjects",
+            logo:"/exposant/integrationObjects.jpg"
+        },
+        {
+            name:"karriery",
+            logo:"/exposant/karriery.jpg"
+        },
+        {
+            name:"KPMG",
+            logo:"/exposant/KPMG.jpg"
+        },
+        {
+            name:"Linedata_logo",
+            logo:"/exposant/Linedata_logo.jpg"
+        },
+        {
+            name:"Logo-Perenco",
+            logo:"/exposant/Logo-Perenco.jpg"
+        },
+        {
+            name:"mantu",
+            logo:"/exposant/mantu.png"
+        },
+        {
+            name:"newAccess",
+            logo:"/exposant/newAccess.jpg"
+        },
+        {
+            name:"Oddo-BHF-Tunis",
+            logo:"/exposant/Oddo-BHF-Tunis.png"
+        },
+        {
+            name:"ONETECH",
+            logo:"/exposant/ONETECH.jpg"
+        },
+        {
+            name:"Orange_logo.svg",
+            logo:"/exposant/Orange_logo.svg.png"
+        },
+        {
+            name:"segitec",
+            logo:"/exposant/segitec.png"
+        },
+        {
+            name:"sofrecom",
+            logo:"/exposant/sofrecom.jpg"
+        },
+        {
+            name:"tankmed",
+            logo:"/exposant/tankmed.jpg"
+        },
+        {
+            name:"telnet",
+            logo:"/exposant/telnet.jpg"
+        },
+        {
+            name:"visteon-vector-logo",
+            logo:"/exposant/visteon-vector-logo.png"
+        },
+        {
+            name:"Visteon",
+            logo:"/exposant/Visteon.jpg"
+        },
+        {
+            name:"WYNSYS",
+            logo:"/exposant/WYNSYS.png"
+        },
+
+
+    ]
     
     return (
         <div className={classes.slider}>
-            <div className={classes.slide_track} style={{ width: `calc(300px * ${exposant.length} ); `}}>
-            {exposant.map((val,key) => {
+            <div className={classes.slide_track} style={{ width: `calc(200px * ${data.length} ); `}}>
+            {/* {exposant.map((val,key) => {
             return(
                 <div className={classes.slide} >
                     <ProfilExposant photo={process.env.PUBLIC_URL + `/exposant/entreprise${val.id}.png`}/>
                 </div>
             );
+          })} */}
+          {data.map((val,key) => {
+            return(
+                <div className={classes.slide} >
+                    <ProfilExposant photo={val.logo}/>
+                </div>
+            );
           })}
-         {exposant.map((val,key) => {
+         {/* {exposant.map((val,key) => {
             return(
                 <div className={classes.slide} >
                     <ProfilExposant photo={process.env.PUBLIC_URL + `/exposant/entreprise${val.id}.png`}/>
                 </div>
             );
-          })}
+          })} */}
          
 
             </div>
